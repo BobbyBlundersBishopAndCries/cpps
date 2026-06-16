081bd50b3647f45755f18b4adf6e68002f8db203
simo <med@Godless77cheikh.localdomain>
2026-06-16 22:06:25 +0100

    some modifications left

--- c09/ex02/PmergeMe.cpp
+++ c09/ex02/PmergeMe.cpp
@@ -1,190 +1,222 @@
 #include "PmergeMe.hpp"
+#include <climits>
 
-// ===== GLOBALS =====
-std::vector<unsigned int> v;
-std::deque<unsigned int> dq;
-
-unsigned int v_straggler = 0;
-bool v_has_straggler = false;
-
-unsigned int dq_straggler = 0;
-bool dq_has_straggler = false;
-unsigned int parse_number(const char *s)
+std::vector<unsigned int> g_vec;
+std::deque<unsigned int> g_deq;
+void parse_args(int ac, char** av)
 {
-    unsigned int r = 0;
-
-    for (size_t i = 0; s[i]; i++)
+    for (int i = 1; i < ac; i++)
     {
-        if (!std::isdigit(s[i]))
-            throw "Error";
-
-        if (r > UINT_MAX / 10 || (r == UINT_MAX / 10 && (s[i] - '0') > 9))
-            throw "Error";
-
-        r = r * 10 + (s[i] - '0');
-    }
-    return r;
-}
-void parse_data(char **av)
-{
-    for (int i = 1; av[i]; i++)
-    {
-        unsigned int n = parse_number(av[i]);
-        v.push_back(n);
-        dq.push_back(n);
-    }
-}
-static std::vector<std::pair<unsigned int, unsigned int> >
-make_pairs(const std::vector<unsigned int> &a)
-{
-    std::vector<std::pair<unsigned int, unsigned int> > res;
-
-    for (size_t i = 0; i + 1 < a.size(); i += 2)
-        res.push_back(std::make_pair(a[i], a[i + 1]));
-
-    return res;
-}
-static void sort_pairs(std::vector<std::pair<unsigned int, unsigned int> > &p)
-{
-    for (size_t i = 0; i < p.size(); i++)
-        if (p[i].first < p[i].second)
-            std::swap(p[i].first, p[i].second);
-}
-static void split_pairs(const std::vector<std::pair<unsigned int, unsigned int> > &p,
-                        std::vector<unsigned int> &big,
-                        std::vector<unsigned int> &small)
-{
-    for (size_t i = 0; i < p.size(); i++)
-    {
-        big.push_back(p[i].first);
-        small.push_back(p[i].second);
-    }
-}
-static void insertion_sort(std::vector<unsigned int> &v)
-{
-    for (size_t i = 1; i < v.size(); i++)
-    {
-        unsigned int key = v[i];
-        int j = i - 1;
-
-        while (j >= 0 && v[j] > key)
+        if (av[i][0] == '\0')
+            throw "Error\n";
+        for (size_t j = 0; av[i][j]; j++)
         {
-            v[j + 1] = v[j];
-            j--;
+            if (!(std::isdigit(av[i][j]) || av[i][0] == '+'))
+                throw "Error\n";
         }
-        v[j + 1] = key;
-    }
-}
-static std::vector<size_t> jacobsthal(size_t n)
-{
-    std::vector<size_t> order;
-    std::vector<bool> used(n, false);
-
-    if (!n) return order;
-
-    order.push_back(0);
-    used[0] = true;
-
-    size_t a = 0, b = 1;
-
-    while (order.size() < n)
-    {
-        size_t next = a + 2 * b;
-
-        for (size_t i = next; i > b; --i)
-        {
-            size_t idx = i - 1;
-            if (idx < n && !used[idx])
-            {
-                used[idx] = true;
-                order.push_back(idx);
-            }
+            long val = std::atol(av[i]);
+            if (val < 0 || val > UINT_MAX)
+                throw "Error\n";
+            g_vec.push_back(static_cast<unsigned int>(val));
+            g_deq.push_back(static_cast<unsigned int>(val));
         }
-
-        a = b;
-        b = next;
-    }
-
-    for (size_t i = 0; i < n; i++)
-        if (!used[i])
-            order.push_back(i);
-
-    return order;
+    if (g_vec.empty()) 
+        throw "Error\n";
 }
-static void binary_insert(std::vector<unsigned int> &a, unsigned int val)
+template <typename T>
+static void print_sequence(const T& container, const std::string& prefix)
 {
-    size_t l = 0, r = a.size();
+    std::cout << prefix << " ";
+    size_t limit = container.size() > 5 ? 5 : container.size();
+    typename T::const_iterator it = container.begin();
+    for (size_t i = 0; i < limit; ++i, ++it)
+        std::cout << *it << " ";
+    if (container.size() > 5)
+        std::cout << "[...]";
+    std::cout << std::endl;
+}
 
-    while (l < r)
+static size_t jacobsthal_order(size_t n)
+{
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+    size_t prev2 = 0;
+    size_t prev1 = 1;
+    size_t current = 1;
+    for (size_t i = 2; i <= n; ++i)
     {
-        size_t m = (l + r) / 2;
-        if (a[m] < val)
-            l = m + 1;
+        current = prev1 + 2 * prev2;
+        prev2 = prev1;
+        prev1 = current;
+    }
+    return current;
+}
+
+void mergeSortPairsVec(std::vector<std::pair<unsigned int, unsigned int> >& pairs, int left, int right)
+{
+    if (left >= right) return;
+    int mid = left + (right - left) / 2;
+    mergeSortPairsVec(pairs, left, mid);
+    mergeSortPairsVec(pairs, mid + 1, right);
+    std::vector<std::pair<int, int> > temp;
+    int i = left, j = mid + 1;
+    while (i <= mid && j <= right)
+    {
+        if (pairs[i].first <= pairs[j].first)
+            temp.push_back(pairs[i++]);
+        else 
+            temp.push_back(pairs[j++]);
+    }
+    while (i <= mid) temp.push_back(pairs[i++]);
+    while (j <= right) temp.push_back(pairs[j++]);
+    for (int k = 0; k < (int)temp.size(); ++k)
+        pairs[left + k] = temp[k];
+}
+
+void mergeInsertSortVec(std::vector<unsigned int>& arr)
+{
+    if (arr.size() < 2) return;
+    std::vector<std::pair<unsigned int, unsigned int> > pairs;
+    int straggler = -1;
+    bool hasStraggler = false;
+    if (arr.size() % 2 != 0)
+    {
+        straggler = arr.back();
+        hasStraggler = true;
+        arr.pop_back();
+    }
+    for (size_t i = 0; i < arr.size(); i += 2)
+    {
+        if (arr[i] > arr[i+1])
+            pairs.push_back(std::make_pair(arr[i], arr[i+1]));
         else
-            r = m;
+            pairs.push_back(std::make_pair(arr[i+1], arr[i]));
     }
-    a.insert(a.begin() + l, val);
-}
-static void insert_small(std::vector<unsigned int> &big,
-                         std::vector<unsigned int> &small)
-{
-    std::vector<size_t> ord = jacobsthal(small.size());
-
-    for (size_t i = 0; i < ord.size(); i++)
-        binary_insert(big, small[ord[i]]);
-}
-void run_vector_sort()
-{
-    std::vector<std::pair<unsigned int, unsigned int> > p = make_pairs(v);
-
-    sort_pairs(p);
-    std::vector<unsigned int> big;
-    std::vector<unsigned int> small;
-    split_pairs(p, big, small);
-    insertion_sort(big);
-    insert_small(big, small);
-    if (v.size() % 2 != 0)
-        binary_insert(big, v.back());
-
-
-    v = big;
-}
-void run_deque_sort()
-{
-    std::vector<unsigned int> tmp(dq.begin(), dq.end());
-    std::vector<std::pair<unsigned int, unsigned int> > p = make_pairs(tmp);
-    sort_pairs(p);
-    std::vector<unsigned int> big;
-    std::vector<unsigned int> small;
-    split_pairs(p, big, small);
-    insertion_sort(big);
-    std::vector<size_t> ord = jacobsthal(small.size());
-
-    for (size_t i = 0; i < ord.size(); i++)
+    mergeSortPairsVec(pairs, 0, pairs.size() - 1);
+    std::vector<unsigned int> mainChain;
+    std::vector<unsigned int> pend;
+    for (size_t i = 0; i < pairs.size(); ++i)
     {
-        if (v.size() % 2 != 0)
-        {
-            unsigned int val = v.back();
-
-            size_t l = 0, r = big.size();
-            while (l < r)
-            {
-                size_t m = (l + r) / 2;
-                if (big[m] < val) l = m + 1;
-                else r = m;
-            }
-            big.insert(big.begin() + l, val);
-        }
-        unsigned int val = small[ord[i]];
-        size_t l = 0, r = big.size();
-        while (l < r)
-        {
-            size_t m = (l + r) / 2;
-            if (big[m] < val) l = m + 1;
-            else r = m;
-        }
-
-        big.insert(big.begin() + l, val);
+        mainChain.push_back(pairs[i].first);
+        pend.push_back(pairs[i].second);
     }
-    dq = std::deque<unsigned int>(big.begin(), big.end());
+    mainChain.insert(mainChain.begin(), pend[0]);
+    std::vector<int> insertionOrder;
+    size_t jacobIndex = 3;
+    size_t lastJacob = 1;
+    while (lastJacob < pend.size())
+    {
+        size_t currentJacob = jacobsthal_order(jacobIndex);
+        size_t start = std::min(currentJacob, pend.size());
+        for (size_t i = start; i > lastJacob; --i)
+            insertionOrder.push_back(i - 1);
+        lastJacob = start;
+        jacobIndex++;
+    }
+
+    for (size_t i = 0; i < insertionOrder.size(); ++i)
+    {
+        int elementToInsert = pend[insertionOrder[i]];
+        std::vector<unsigned int>::iterator it = std::lower_bound(mainChain.begin(), mainChain.end(), elementToInsert);
+        mainChain.insert(it, elementToInsert);
+    }
+    if (hasStraggler) {
+        std::vector<unsigned int>::iterator it = std::lower_bound(mainChain.begin(), mainChain.end(), straggler);
+        mainChain.insert(it, straggler);
+    }
+    arr = mainChain;
+}
+
+void mergeSortPairsDeq(std::deque<std::pair<unsigned int, unsigned int> >& pairs, int left, int right) {
+    if (left >= right) return;
+    int mid = left + (right - left) / 2;
+    mergeSortPairsDeq(pairs, left, mid);
+    mergeSortPairsDeq(pairs, mid + 1, right);
+
+    std::deque<std::pair<int, int> > temp;
+    int i = left, j = mid + 1;
+    while (i <= mid && j <= right)
+    {
+        if (pairs[i].first <= pairs[j].first)
+            temp.push_back(pairs[i++]);
+        else 
+            temp.push_back(pairs[j++]);    
+    }
+    while (i <= mid) temp.push_back(pairs[i++]);
+    while (j <= right) temp.push_back(pairs[j++]);
+    for (int k = 0; k < (int)temp.size(); ++k)
+        pairs[left + k] = temp[k];
+}
+
+void mergeInsertSortDeq(std::deque<unsigned int>& arr)
+{
+    if (arr.size() < 2) return;
+    std::deque<std::pair<unsigned int, unsigned int> > pairs;
+    int straggler = -1;
+    bool hasStraggler = false;
+    if (arr.size() % 2 != 0)
+    {
+        straggler = arr.back();
+        hasStraggler = true;
+        arr.pop_back();
+    }
+    for (size_t i = 0; i < arr.size(); i += 2)
+    {
+        if (arr[i] > arr[i+1]) 
+            pairs.push_back(std::make_pair(arr[i], arr[i+1]));
+        else 
+            pairs.push_back(std::make_pair(arr[i+1], arr[i]));
+    }
+    mergeSortPairsDeq(pairs, 0, pairs.size() - 1);
+    std::deque<unsigned int> mainChain;
+    std::deque<unsigned int> pend;
+    for (size_t i = 0; i < pairs.size(); ++i)
+    {
+        mainChain.push_back(pairs[i].first);
+        pend.push_back(pairs[i].second);
+    }
+    mainChain.insert(mainChain.begin(), pend[0]);
+    std::deque<int> insertionOrder;
+    size_t jacobIndex = 3;
+    size_t lastJacob = 1;
+    while (lastJacob < pend.size())
+    {
+        size_t currentJacob = jacobsthal_order(jacobIndex);
+        size_t start = std::min(currentJacob, pend.size());
+        for (size_t i = start; i > lastJacob; --i)
+            insertionOrder.push_back(i - 1);
+        lastJacob = start;
+        jacobIndex++;
+    }
+    for (size_t i = 0; i < insertionOrder.size(); ++i)
+    {
+        int elementToInsert = pend[insertionOrder[i]];
+        std::deque<unsigned int>::iterator it = std::lower_bound(mainChain.begin(), mainChain.end(), elementToInsert);
+        mainChain.insert(it, elementToInsert);
+    }
+    if (hasStraggler)
+    {
+        std::deque<unsigned int>::iterator it = std::lower_bound(mainChain.begin(), mainChain.end(), straggler);
+        mainChain.insert(it, straggler);
+    }
+    arr = mainChain;
+}
+
+void run()
+{
+    print_sequence(g_vec, "Before:");
+    struct timeval start, end;
+    gettimeofday(&start, NULL);
+    mergeInsertSortVec(g_vec);
+    gettimeofday(&end, NULL);
+    double timeVec = (end.tv_sec - start.tv_sec) * 1000000.0 + (end.tv_usec - start.tv_usec);
+    struct timeval dstart, dend;
+    gettimeofday(&dstart, NULL);
+    mergeInsertSortDeq(g_deq);
+    gettimeofday(&dend, NULL);
+    double timeDeq = (dend.tv_sec - dstart.tv_sec) * 1000000.0 + (dend.tv_usec - dstart.tv_usec);
+    print_sequence(g_vec, "After: ");
+    std::cout << std::fixed << std::setprecision(5);
+    std::cout << "Time to process a range of " << g_vec.size() << " elements with std::vector : " << timeVec << " us" << std::endl;
+    std::cout << "Time to process a range of " << g_deq.size() << " elements with std::deque  : " << timeDeq << " us" << std::endl;
 }
--- c09/ex02/main.cpp
+++ c09/ex02/main.cpp
@@ -1,54 +1,19 @@
 #include "PmergeMe.hpp"
 
-int main(int ac, char **av)
+int main(int argc, char **argv)
 {
-    if (ac < 2)
-    {
-        std::cerr << "Error\n";
-        return 1;
-    }
-
     try
     {
-        parse_data(av);
+        if (argc < 2)
+            throw "Error";
 
-        std::cout << "Before: ";
-        for (size_t i = 0; i < v.size(); i++)
-            std::cout << v[i] << " ";
-        std::cout << "\n";
-
-        clock_t start = clock();
-        run_vector_sort();
-        clock_t end = clock();
-
-        double t_vec = (double)(end - start) * 1000000 / CLOCKS_PER_SEC;
-
-        start = clock();
-        run_deque_sort();
-        end = clock();
-
-        double t_deq = (double)(end - start) * 1000000 / CLOCKS_PER_SEC;
-
-        std::cout << "After: ";
-        for (size_t i = 0; i < v.size(); i++)
-            std::cout << v[i] << " ";
-        std::cout << "\n";
-
-        std::cout << "Time to process a range of " << v.size()
-                  << " elements with std::vector : "
-                  << std::fixed << std::setprecision(5)
-                  << t_vec << " us\n";
-
-        std::cout << "Time to process a range of " << dq.size()
-                  << " elements with std::deque : "
-                  << std::fixed << std::setprecision(5)
-                  << t_deq << " us\n";
-        v.clear();
-        dq.clear();
+        parse_args(argc, argv);
+        run();
     }
-    catch (...)
+    catch (const char *msg)
     {
-        std::cerr << "Error\n";
-        return 1;
+        std::cerr << msg;
+        return (1);
     }
+    return (0);
 }
